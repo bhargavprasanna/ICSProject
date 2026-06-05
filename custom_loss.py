9f0dc00e91464b46fbed9e4b119af53b42839280
Bhargav Prasanna <ubhargavprasanna@gmail.com>
2026-06-05 12:57:10 +0530

    Update custom_loss.py

--- custom_loss.py
+++ custom_loss.py
@@ -1,1 +1,22 @@
 
+import tensorflow as tf
+def focal_loss(gamma=2.0, alpha=0.25):
+    def loss(y_true, y_pred):
+        y_pred = tf.clip_by_value( y_pred,1e-7,1 - 1e-7 )
+
+        cross_entropy = -y_true * tf.math.log(y_pred)
+
+        focal = (
+            alpha *
+            tf.pow(1 - y_pred, gamma) *
+            cross_entropy
+        )
+
+        return tf.reduce_mean(
+            tf.reduce_sum(
+                focal,
+                axis=1
+            )
+        )
+
+    return loss
